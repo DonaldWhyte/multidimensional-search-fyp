a669c766c5bff2db3e2ee0b28d743a609a360cca
Donald Whyte <dwhyte@helios.(none)>
2014-03-06 16:43:40 +0000

    Wrote CMake project structure for data generators/loaders, index structures, main evaluator and unit tests that will use the google-test library

---
+++ evaluator/src/EvaluatorMain.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Nothing to evaluate." << std::endl;
+	return 0;
+}
---
+++ index_structures/tests/TestRunner.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "No tests to run." << std::endl;
+	return 0;
+}
