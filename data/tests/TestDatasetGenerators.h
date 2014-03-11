f68c26c05b5da5cc0d024f0139bf835659fb3f58
Donald Whyte <dwhyte@helios.(none)>
2014-03-11 19:45:04 +0000

    Renamed HyperCubeRandomGenerator class and now use Region instances instead of min/max points

--- data/tests/TestDatasetGenerators.h
+++ data/tests/TestDatasetGenerators.h
@@ -3,7 +3,7 @@
 
 #include <gtest/gtest.h>
 #include "UniformDatasetGenerator.h"
-#include "HyperCubeRandomGenerator.h"
+#include "RandomPointGenerator.h"
 #include "SkewedDatasetGenerator.h"
 
 namespace mdsearch { namespace tests
@@ -99,19 +99,19 @@ namespace mdsearch { namespace tests
 		EXPECT_EQ(twoDInterval3P, generator.generate(2, 10, 20, 3));
 	}
 
-	TEST_F(DatasetGeneratorTests, HyperCubeRandomGenerator)
+	TEST_F(DatasetGeneratorTests, RandomPointsInRegions)
 	{
-		HyperCubeRandomGenerator generator;
+		RandomPointGenerator generator;
+		// Construct test data
+		Region region1D(1, Interval(1, 5));
+		Region region10D(10, Interval(2, 4));
+		region10D[2].min = 3;
+		region10D[6].max = 5.5; // make boundary point coordinates not all same value
+		Region zeroSizedRegion(1, Interval(8, 8));
 
-		Point minPoint1D(1, 1);
-		Point maxPoint1D(1, 5);
-		Point minPoint10D(10, 2);
-		minPoint10D[2] = 3;
-		Point maxPoint10D(10, 4);
-		maxPoint10D[6] = 5.5; // make boundary point coordinates not all same value
 		// Generate a collection of 1D and 10D points
-		PointList oneDPoints = generator.generate(1, minPoint1D, maxPoint1D, NUM_RANDOM_POINTS_TO_GENERATE);
-		PointList tenDPoints = generator.generate(10, minPoint10D, maxPoint10D, NUM_RANDOM_POINTS_TO_GENERATE);
+		PointList oneDPoints = generator.generatePointsInRegion(1, region1D, NUM_RANDOM_POINTS_TO_GENERATE);
+		PointList tenDPoints = generator.generatePointsInRegion(10, region10D, NUM_RANDOM_POINTS_TO_GENERATE);
 		// Ensure enough points were returned
 		ASSERT_EQ(NUM_RANDOM_POINTS_TO_GENERATE, oneDPoints.size());
 		ASSERT_EQ(NUM_RANDOM_POINTS_TO_GENERATE, tenDPoints.size());
@@ -122,24 +122,24 @@ namespace mdsearch { namespace tests
 		for (PointList::const_iterator it = oneDPoints.begin(); (it != oneDPoints.end()); it++)	
 		{
 			const Point& p = *it;
-			ASSERT_GE(p[0], minPoint1D[0]);
-			ASSERT_LE(p[0], maxPoint1D[0]);
+			ASSERT_GE(p[0], region1D[0].min);
+			ASSERT_LE(p[0], region1D[0].max);
 		}
 		for (PointList::const_iterator it = tenDPoints.begin(); (it != tenDPoints.end()); it++)	
 		{
 			const Point& p = *it;
 			for (unsigned int d = 0; (d < 10); d++)
 			{
-				ASSERT_GE(p[d], minPoint10D[d]);
-				ASSERT_LE(p[d], maxPoint10D[d]);
+				ASSERT_GE(p[d], region10D[d].min);
+				ASSERT_LE(p[d], region10D[d].max);
 			}
 		}
 
 		// Test with zero-sized hyper-cube (same point should always be generated)
-		PointList samePoints = generator.generate(1, minPoint1D, minPoint1D, 10);
+		PointList samePoints = generator.generatePointsInRegion(1, zeroSizedRegion, 10);
 		for (PointList::const_iterator it = samePoints.begin(); (it != samePoints.end()); it++)	
 		{
-			ASSERT_EQ(minPoint1D, *it);
+			ASSERT_EQ(zeroSizedRegion[0].max, (*it)[0]);
 		}
 	}
 
