bff4fde283bff217bf0e3d574e0656dda3b949fc
Donald Whyte <dwhyte@helios.(none)>
2014-03-12 15:47:26 +0000

    Integrated command line parser into the evaulator's main() function

--- evaluator/src/EvaluatorMain.cpp
+++ evaluator/src/EvaluatorMain.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include "CommandLineArguments.h"
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Nothing to evaluate." << std::endl;
-	for (unsigned int i = 0; (i < argc); i++)
-		std::cout << "\"" << argv[i] << "\", " << std::endl;
-	std::cout << std::endl;
+	// Parse command line arguments
+	mdsearch::CommandLineArguments args(argc, argv);
+	// If the arguments are not valid, exit program with failure code 
+	if (!args.isValid())
+	{
+		std::cout << "Invalid command line arguments (use -h or --help for accepted arguments)." << std::endl;
+		return 1;
+	}
+
+	std::cout << "Not implemented yet." << std::endl;
+
 	return 0;
 }
