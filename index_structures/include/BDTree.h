a8d79927c9900078fabc54a39b9bebc04b74e2df
Donald Whyte <dwhyte@helios.(none)>
2014-03-19 10:25:57 +0000

    Committing current progress on BDTree

--- index_structures/include/BDTree.h
+++ index_structures/include/BDTree.h
@@ -6,10 +6,76 @@
 namespace mdsearch
 {
 
+	enum NodeRelation
+	{
+		NODE_RELATION_NONE = 0,
+		NODE_RELATION_LEFT,
+		NODE_RELATION_RIGHT,
+		NODE_RELATION_OUTER
+	};
+
 	class BDTree : public IndexStructure
 	{
 
 	public:
+		enum NodeType
+		{
+			EMPTY_NODE = 0,
+			SPLIT_NODE,
+			SHRINK_NODE,
+			LEAF_POINT_NODE,
+			LEAF_BOX_NODE
+		};
+
+		struct Node
+		{
+			Node(NodeType type);
+			virtual ~Node();
+
+			virtual bool contains(const Point& p) = 0;
+
+			NodeType type;
+		};
+
+		struct SplitNode : public Node
+		{
+			SplitNode();
+			virtual ~SplitNode();
+			bool contains(const Point& p);
+
+			Region innerBox; // overall boundary of split node
+			Node* leftChild;
+			Node* rightChild; 
+		};
+
+		struct ShrinkNode : public Node
+		{
+			ShrinkNode();
+			virtual ~ShrinkNode();
+			bool contains(const Point& p);
+
+			SplitNode* innerChild; // containers inner region
+			Node* outerChild; // contains outer region
+			// NOTE: use both to compute set-theoretic difference
+			// of two to get outerChild's actual region
+		};		
+
+		struct PointNode : public Node
+		{
+			PointNode(const Point& point);
+			bool contains(const Point& p);
+
+			Point point;
+		};
+
+		struct InnerBoxNode : public Node
+		{
+			InnerBoxNode(const Region& boxRegion);
+			bool contains(const Point& p);
+
+			Region innerBox;
+		};
+
 		BDTree(unsigned int numDimensions, const Region& boundary);
 
 		virtual void clear();
@@ -22,25 +88,20 @@ namespace mdsearch
 		virtual PointList pointsInRegion(const Region& region);
 
 	private:
-		struct BDTreeNode
-		{
-			Region boundary;
-			Region otherBoundary;
-			bool differenceInBoundaries;
-
-			BDTreeNode* left;
-			BDTreeNode* right;
-			BDTreeNode* outer;
-
-			Point point;
-		};
-
+		/* Partition node region using orthogonal hyperlane to its longest size,
+		 * creating two children. */
+		SplitNode* split(Node* node);
+		/* Partition node region into two children. The left child contains
+		 * the node's inner box and the right is the set theoretic different
+		 * of the left node's shrunk region and the original node's region
+		 * (this is known as the outer box). */
+		ShrinkNode* shrink(Node* node);
 		/* Perform a Splay quadtree promote(x) operation on the given node.
 		 * Return true if the node was successfully promoted and false
 		 * if it wasn't (due to it already being the root). */
-		bool promote(BDTreeNode* node);
+		bool promote(Node* node);
 
-		BDTreeNode* root;
+		Node* root;
 
 	};
 
--- index_structures/src/BDTree.cpp
+++ index_structures/src/BDTree.cpp
@@ -3,20 +3,73 @@
 namespace mdsearch
 {
 
-	BDTree::BDTree(unsigned int numDimensions, const Region& boundary) :
-		IndexStructure(numDimensions), boundary(boundary)
+	BDTree::Node::Node(BDTree::NodeType type) : type(type) { }
+	BDTree::Node::~Node() { }
+
+	BDTree::SplitNode::SplitNode() : Node(BDTree::SPLIT_NODE) { }
+	BDTree::SplitNode::~SplitNode()
 	{
-		
+		delete leftChild;
+		delete rightChild;
+	}
+	bool BDTree::SplitNode::contains(const Point& p)
+	{
+		return (leftChild->contains(p) || rightChild->contains(p));
+	}
+
+	BDTree::ShrinkNode::ShrinkNode() : Node(BDTree::SHRINK_NODE) { }
+	BDTree::ShrinkNode::~ShrinkNode()
+	{
+		delete innerChild;
+		delete outerChild;
+	}
+	bool BDTree::ShrinkNode::contains(const Point& p)
+	{
+		return (innerChild->contains(p) || outerChild->contains(p));
+	}
+
+	BDTree::PointNode::PointNode(const Point& point) :
+		Node(BDTree::LEAF_POINT_NODE), point(point) { }
+	bool BDTree::PointNode::contains(const Point& p)
+	{
+		return (p == point);
+	}
+
+	BDTree::InnerBoxNode::InnerBoxNode(const Region& boxRegion) :
+		 Node(BDTree::LEAF_BOX_NODE), innerBox(boxRegion) { }
+	bool BDTree::InnerBoxNode::contains(const Point& p)
+	{
+		return innerBox.contains(p);
+	}
+
+	BDTree::BDTree(unsigned int numDimensions, const Region& boundary) :
+		IndexStructure(numDimensions), root(NULL)
+	{
+		clear();
 	}
 
 	void BDTree::clear()
 	{
-		// TODO
+		// Simply delete the root node and replace it with a new rooy
+		delete root;
+		// Construct empty inner box node for root
+		root = new InnerBoxNode(boundary);
 	}
 
 	bool BDTree::insert(const Point& p)
 	{
-		// TODO
+		// TODO: find leaf node u that contains p
+
+
+		// TODO: let b be u's contents (left node??) and C be u's outer box
+
+		// TODO: compute minimum quadtree box E that encloses both p and b
+
+		// TODO: split E to produce two nodes v and w
+		// v is a node which shrinks from u's outer box to E -- THIS REPLACES u!
+		// w is an INNER CHILD OF V, which is a split node that separates b from p
+		// by inner-left-convention, the LEFT CHILD of w contains b and the right child contains p
+
 		return false;
 	}
 
@@ -25,10 +78,18 @@ namespace mdsearch
 		// TODO
 		return false;
 	}
+
 	bool BDTree::update(const Point& oldPoint, const Point& newPoint)
 	{
-		// TODO
-		return false;
+		if (remove(oldPoint))
+		{
+			insert(newPoint);
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	bool BDTree::pointExists(const Point& p)
@@ -41,6 +102,23 @@ namespace mdsearch
 	{
 		// TODO
 		return PointList();
+	}
+
+	BDTree::SplitNode* BDTree::split(BDTree::Node* node)
+	{
+		// TODO
+		return NULL;
+	}
+
+	BDTree::ShrinkNode* BDTree::shrink(BDTree::Node* node)
+	{
+		// TODO
+		return NULL;
+	}
+
+	bool BDTree::promote(BDTree::Node* node)
+	{
+		// TODO
 	}
 
 }
