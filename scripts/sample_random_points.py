8abc9c4b7d5dd0c45925afa3f582a5ddf47f0810
Donald Whyte <sc10dw@cslin041.csunix.comp.leeds.ac.uk>
2014-04-03 18:55:57 +0100

    Modified point sampling script so it doesn't store whole file in memory, it streams/reads it as it goes. Also modified it1 run script to consider ne astrophysics data

--- scripts/sample_random_points.py
+++ scripts/sample_random_points.py
@@ -4,23 +4,53 @@ import time
 from generate_dataset_operations import loadDataset
 from generate_random_dataset import pointToStr
 
-def randomlySampleDataset(dataset, pointsToSample):
-	fullDataset = list(dataset) # copied since it will modified
-	sampledDataset = []
-	for i in range(pointsToSample):
-		# Generate random index into full dataset and place
-		# point at given index into sampled dataset
-		index = random.randint(0, len(fullDataset) - 1)
-		sampledDataset.append(fullDataset[index])
-		# Remove selected point from the full dataset so it
-		# won't be chosen again
-		del fullDataset[index]
-	return sampledDataset
+def generateRandomIndices(numPoints, pointsToSample):
+	# Just in case points to sample is larger than available points
+	pointsToSample = min(pointsToSample, numPoints)
 
-def outputDataset(numDimensions, dataset):
-	print("{} {}".format(numDimensions, len(dataset)))
-	for ds in dataset:
-		print(pointToStr(ds))
+	allIndices = list(range(numPoints))
+	chosenIndices = []
+	for i in range(pointsToSample):
+		# Generate random index into dataset index list
+		# and store dataset index
+		index = random.randint(0, len(allIndices) - 1)
+		chosenIndices.append(allIndices[index])
+		# Remove selected index  so it won't be chosen again
+		del allIndices[index]
+	chosenIndices.sort()
+	return chosenIndices
+
+def sampleDataset(datasetFilename, pointsToSample):
+	with open(datasetFilename, "r") as f:
+		# Read header line to get dimensions and number of points
+		line = f.readline()
+		fields = line.split()
+		if len(fields) < 2:
+			raise RuntimeError("Invalid file header")
+		numDimensions = int(fields[0])
+		numPoints = int(fields[1])
+		# Generate random sorted list of point indices which indicate
+		# which points to sample
+		indices = generateRandomIndices(numPoints, pointsToSample)
+
+		# Output new header file
+		print("{} {}".format(numDimensions, len(indices)))
+		# Now go through each line, outputting points to sample
+		currentLine = 0
+		nextIndexToSample = indices.pop(0)
+		line = f.readline()
+		while line != "":
+			if currentLine == nextIndexToSample:
+				# Parse all fields of into floats and output new format
+				point = [float(x) for x in line[:-1].split()] # [:-1] to remove trailing newline
+				print(pointToStr(point))
+				if len(indices) == 0: # no more points to sample, break the loop!
+					break				
+				else:
+					nextIndexToSample = indices.pop(0) # otherwise, get next index to sample
+			
+			line = f.readline()
+			currentLine += 1
 
 if __name__ == "__main__":
 	random.seed(time.time())
@@ -31,8 +61,4 @@ if __name__ == "__main__":
 	datasetFilename = sys.argv[1]
 	pointsToSample = int(sys.argv[2])
 
-	numDimensions, numPoints, dataset = loadDataset(datasetFilename)
-	# Done in case there are less points in dataset than points to sample
-	pointsToSample = min(pointsToSample, len(dataset))
-	dataset = randomlySampleDataset(dataset, pointsToSample)
-	outputDataset(numDimensions, dataset)
+	sampleDataset(datasetFilename, pointsToSample)
