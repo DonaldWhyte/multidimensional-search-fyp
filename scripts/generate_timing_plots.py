35acebd983df80fba7bebaf90e5e14592a86c5d2
Donald Whyte <sc10dw@cslin017.csunix.comp.leeds.ac.uk>
2014-04-20 21:39:53 +0100

    Made it so generate plot script can process multiple timing files using GLOB pattersn

--- scripts/generate_timing_plots.py
+++ scripts/generate_timing_plots.py
@@ -1,4 +1,5 @@
 import sys
+import glob
 from timing_file_parser import parseTimingFile
 
 import matplotlib.pyplot as plt
@@ -21,11 +22,10 @@ class Dataset:
 
 	def plot(self, plot):
 		xValues, yValues = extract2DCoordinates(self.points)
-		print self.name, self.points
 		return plot.plot(xValues, yValues, color=self.colour, marker=self.marker)
 
 def generatePlot(filename, xAxisLabel, yAxisLabel, operation, structures):
-	# Maintain sorted listgof structures and generate dataset object
+	# Maintain sorted list of structures and generate dataset object
 	# for each structure
 	structureNames = sorted(structures.keys())
 	datasets = []
@@ -54,11 +54,15 @@ def generatePlot(filename, xAxisLabel, yAxisLabel, operation, structures):
 if __name__ == "__main__":
 	# Parse command line options
 	if len(sys.argv) < 2:
-		sys.exit("python %s <timingFilename>" % sys.argv[0])
-	timingFilename = sys.argv[1]
-	# Retrieve contents of timing file
-	data = parseTimingFile(timingFilename)
-	# Generate a plot for each operation
-	for operation, structures in data.data.items():
-		filename = "%s_%s.pdf" % (data.title, operation)
-		generatePlot(filename, data.xAxis, data.yAxis, operation, structures)
+		sys.exit("python %s \"<timingFilenameGlob>\"" % sys.argv[0])
+	timingFilenameGlob = sys.argv[1]
+	# Use glob to find all desired files
+	filenames = glob.glob(timingFilenameGlob)
+	# Process each timing file in turn
+	for fname in filenames:
+		# Retrieve contents of timing file
+		data = parseTimingFile(fname)
+		# Generate a plot for each operation
+		for operation, structures in data.data.items():
+			outputFilename = "%s_%s.pdf" % (data.title, operation)
+			generatePlot(outputFilename, data.xAxis, data.yAxis, operation, structures)
