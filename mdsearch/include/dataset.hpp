d7b39c50b07d59212a8fc4eaa31cc8f1a980d97c
Donald Whyte <sc10dw@cslin246.csunix.comp.leeds.ac.uk>
2014-05-07 11:08:52 +0100

    Chnaged extensions of all header files to *.hpp. Added comment headers with MIT licence to each file too

--- mdsearch/include/dataset.hpp
+++ mdsearch/include/dataset.hpp
@@ -1,7 +1,41 @@
+/******************************************************************************
+
+mdsearch - Lightweight C++ library implementing a collection of
+           multi-dimensional search structures
+
+File:        dataset.hpp
+Description: TODO
+
+*******************************************************************************
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Donald Whyte
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+******************************************************************************/
+
 #ifndef MDSEARCH_DATASET_H
 #define MDSEARCH_DATASET_H
 
-#include "types.h"
+#include "types.hpp"
 #include <boost/lexical_cast.hpp>
 #include <fstream>
 
